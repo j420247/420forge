b0d1f0ccf75a70443c33d1ad13436e39c5e46b73
Brett Meehan <bmeehan@atlassian.com>
2018-01-04 08:48:45 +1100

    consolidated cookie storage for navbar values

--- acforge.py
+++ acforge.py
@@ -6,6 +6,7 @@ from flask_bootstrap import Bootstrap
 import os
 import json
 import boto3
+from pprint import pprint
 
 # configuration
 DATABASE = 'acforge.db'
@@ -18,6 +19,8 @@ PASSWORD = 'admin'
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config.from_object(__name__)
+# set defaults to 'upgrade' and 'stg'
+saved_data = {'environment': 'stg', 'forgetype': 'upgrade'}
 
 def get_cfn_stacks_for_environment(env):
     if env == 'prod':
@@ -28,61 +31,51 @@ def get_cfn_stacks_for_environment(env):
     stack_list = cfn.stacks.all()
     return stack_list
 
+def put_saved_data(ckey,cvalue):
+    response = make_response(redirect(url_for('index')))
+    saved_data = get_saved_data()
+    saved_data[ckey] = cvalue
+    response.set_cookie('forgedata', json.dumps(saved_data))
+    return response
 
 def get_saved_data():
     try:
         saved_data = json.loads(request.cookies.get('forgedata'))
     except TypeError:
         saved_data = { }
+    pprint(saved_data)
     return saved_data
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    response = make_response(redirect(url_for('upgrade')))
-    # set defaults
-    response.set_cookie('forgetype', 'upgrade')
-    response.set_cookie('environment', 'stg')
+    get_cfn_stacks_for_environment('stg')
     return render_template('index.html', saves=get_saved_data())
 
 @app.route('/upgrade.html', methods=['GET', 'POST'])
 def upgrade():
-    response = make_response(redirect(url_for('upgrade')))
-    response.set_cookie('forgetype', 'upgrade')
-    return render_template('upgrade.html', saves=get_saved_data())
+    response = put_saved_data('forgetype', 'upgrade')
+    return response
 
 @app.route('/clone.html', methods=['GET', 'POST'])
 def clone():
-    response = make_response(redirect(url_for('clone')))
-    response.set_cookie('forgetype', 'clone')
-    return render_template('clone.html', saves=get_saved_data())
+
+    response = put_saved_data('forgetype', 'clone')
+    return response
 
 @app.route('/restart.html', methods=['GET', 'POST'])
 def restart():
-    response = make_response(redirect(url_for('restart')))
-    response.set_cookie('forgetype', 'restart')
-    return render_template('restart.html', saves=get_saved_data())
+    response = put_saved_data('forgetype', 'restart')
+    return response
 
 @app.route('/stg.html', methods=['GET', 'POST'])
 def stg():
-    get_cfn_stacks_for_environment('stg')
-    response = make_response(redirect(url_for('stg')))
-    response.set_cookie('environment', 'stg')
-    return render_template('stg.html', saves=get_saved_data())
+    response = put_saved_data('environment', 'stg')
+    return response
 
 @app.route('/prod.html', methods=['GET', 'POST'])
 def prod():
-    response = make_response(redirect(url_for('prod')))
-    response.set_cookie('environment', 'prod')
-    return render_template('stg.html', saves=get_saved_data())
-
-@app.route('/settype', methods=['POST'])
-def settype():
-    response = make_response(redirect(url_for('index')))
-    saved_data = get_saved_data()
-    saved_data.update(dict(request.form.items()))
-    response.set_cookie('forgedata', json.dumps(saved_data))
+    response = put_saved_data('environment', 'prod')
     return response
-
 
 if __name__ == '__main__':
     app.run(debug=True)
