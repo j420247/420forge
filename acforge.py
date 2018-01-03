d1e78561af48c37f6eb6714f231a883389beecf9
Brett Meehan <bmeehan@atlassian.com>
2018-01-03 14:45:59 +1100

    AUI'ing shite better #4

--- acforge.py
+++ acforge.py
@@ -27,19 +27,41 @@ def get_saved_data():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    response = make_response(redirect(url_for('upgrade')))
+    # set defaults
+    response.set_cookie('forgetype', 'upgrade')
+    response.set_cookie('environment', 'stg')
     return render_template('index.html', saves=get_saved_data())
 
 @app.route('/upgrade.html', methods=['GET', 'POST'])
 def upgrade():
+    response = make_response(redirect(url_for('upgrade')))
+    response.set_cookie('forgetype', 'upgrade')
     return render_template('upgrade.html', saves=get_saved_data())
 
 @app.route('/clone.html', methods=['GET', 'POST'])
 def clone():
+    response = make_response(redirect(url_for('clone')))
+    response.set_cookie('forgetype', 'clone')
     return render_template('clone.html', saves=get_saved_data())
 
 @app.route('/restart.html', methods=['GET', 'POST'])
 def restart():
+    response = make_response(redirect(url_for('restart')))
+    response.set_cookie('forgetype', 'restart')
     return render_template('restart.html', saves=get_saved_data())
+
+@app.route('/stg.html', methods=['GET', 'POST'])
+def stg():
+    response = make_response(redirect(url_for('stg')))
+    response.set_cookie('environment', 'stg')
+    return render_template('stg.html', saves=get_saved_data())
+
+@app.route('/prod.html', methods=['GET', 'POST'])
+def prod():
+    response = make_response(redirect(url_for('prod')))
+    response.set_cookie('environment', 'prod')
+    return render_template('stg.html', saves=get_saved_data())
 
 @app.route('/settype', methods=['POST'])
 def settype():
