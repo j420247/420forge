ccc15bc12c15facdf74fbcb96d88116e6b8df9b9
Brett Meehan <bmeehan@atlassian.com>
2018-01-03 15:08:27 +1100

    AUI'ing shite better #5

--- acforge.py
+++ acforge.py
@@ -3,6 +3,7 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, jsonify, make_response
 from flask_bootstrap import Bootstrap
+import os
 import json
 import boto3
 
@@ -17,6 +18,16 @@ PASSWORD = 'admin'
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config.from_object(__name__)
+
+def get_cfn_stacks_for_environment(env):
+    if env == 'prod':
+        region = 'us-west-2'
+    else:
+        region = 'us-east-1'
+    cfn = boto3.resource('cloudformation', region_name=region)
+    stack_list = cfn.stacks.all()
+    return stack_list
+
 
 def get_saved_data():
     try:
@@ -53,6 +64,7 @@ def restart():
 
 @app.route('/stg.html', methods=['GET', 'POST'])
 def stg():
+    get_cfn_stacks_for_environment('stg')
     response = make_response(redirect(url_for('stg')))
     response.set_cookie('environment', 'stg')
     return render_template('stg.html', saves=get_saved_data())
