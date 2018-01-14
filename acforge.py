77b462b11c033dfd2c618d3c70ea50582d19ab2c
Brett Meehan <bmeehan@atlassian.com>
2018-01-14 16:10:15 +1100

    logging by progress_log function for checking with /status

--- acforge.py
+++ acforge.py
@@ -3,6 +3,7 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, jsonify, make_response
 from flask_restful import reqparse, abort, Api, Resource
+from datetime import datetime
 import requests
 import os
 import json
@@ -22,16 +23,53 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 api = Api(app)
 
+##
+#### Endpoint classes
+##
+
 class hello(Resource):
     def get(self):
         return {'hello': 'world'}
 
 class testing(Resource):
+    def get(self, env, stack, newversion):
+        session['progress_log'] = "beginning testing " + str(datetime.now())
+        session['action'] = "status"
+        session['environment'] = env
+        session['stack'] = stack
+        session['newversion'] = newversion
+        if env == 'prod':
+            session['region'] = 'us-west-2'
+        else:
+            session['region'] = 'us-east-1'
+        get_stack_current_state()
+        spinup_to_one_appnode()
+        # dummy up the numbers
+        session['appnodemin'] = '2'
+        session['appnodemax'] = '6'
+        session['syncnodemin'] = '2'
+        session['syncnodemax'] = '6'
+        pprint(session)
+        #        if originial stack had more than one node spin up to same as before
+        if session['appnodemin'] != "1":
+            spinup_remaining_nodes()
+        elif 'syncnodemin' in session.keys() and session['syncnodemin'] != "1":
+            spinup_remaining_nodes()
+        # validate_service_responding()
+        print("final state")
+        spl = session['progress_log']
+        print(spl)
+        return(session['progress_log'])
+
+class clear(Resource):
     def get(self):
-        return "testing something"
+        session.clear()
+        session['progress_log'] = str ( "log cleared " + str(datetime.now()))
+        return "session cleared"
 
 class upgrade(Resource):
     def get(self, env, stack, newversion):
+        session['progress_log'] = ("beginning upgrade " + str(datetime.now()))
         session['action'] = "upgrade"
         session['environment'] = env
         session['stack'] = stack
@@ -47,32 +85,38 @@ class upgrade(Resource):
         spindown_to_zero_appnodes()
         # spin stack up to 1 node on new release version
         spinup_to_one_appnode()
-        # wait for service to respond
-        # spinup remaining appnodes in stack
+        # spinup remaining appnodes in stack if needed
+        if session['appnodemin'] != "1":
+            spinup_remaining_nodes()
+        elif 'syncnodemin' in session.keys() and session['syncnodemin'] != "1" :
+            spinup_remaining_nodes()
         # wait for remaining nodes to respond ???
         # enable traffic at VTM
-
-        return [ "starting upgrade for " + stack +" at " + env + " to version " + newversion ]
+        progress_log([ "completed upgrade for " + stack +" at " + env + " to version " + newversion])
+        print("final state")
+        spl = session['progress_log']
+        print(spl)
+        return(session['progress_log'])
     def put(self, env, stack):
         return {env: stack}
 
 class status(Resource):
-    def get(self, env, stack):
-        session['action'] = "status"
-        session['environment'] = env
-        session['stack'] = stack
-        get_stack_current_state()
-        validate_service_responding()
-        return "something is happening"
+    def get(self):
+        return(session['progress_log'])
 
 
 api.add_resource(hello, '/hello')
-api.add_resource(testing, '/test')
+api.add_resource(testing, '/test/<string:env>/<string:stack>/<string:newversion>')
+api.add_resource(clear, '/clear')
 api.add_resource(upgrade, '/upgrade/<string:env>/<string:stack>/<string:newversion>')
-api.add_resource(status, '/status/<string:env>/<string:stack>')
+api.add_resource(status, '/status')
+
+##
+#### stack action functions
+##
 
 def get_stack_current_state():
-    print("getting pre-upgrade stack state")
+    progress_log("getting pre-upgrade stack state")
     # store outcome in session
     cfn = boto3.client('cloudformation', region_name=session['region'])
     stack_details = cfn.describe_stacks( StackName=session['stack'] )
@@ -95,19 +139,19 @@ def get_stack_current_state():
         session['syncnodemin'] = [p['ParameterValue'] for p in stack_details['Stacks'][0]['Parameters'] if
                                p['ParameterKey'] == 'SynchronyClusterNodeMin'][0]
     except:
-        print("not confluence")
+        progress_log("not confluence")
 
     #jira
     try:
         session['preupgrade_jira_version'] = \
         [p['ParameterValue'] for p in stack_details['Stacks'][0]['Parameters'] if p['ParameterKey'] == 'JiraVersion'][0]
     except:
-        print("not jira")
-    pprint(session)
+        progress_log("not jira")
+    progress_log(str(session))
     return
 
 def spindown_to_zero_appnodes():
-    print("spinning stack down to 0 nodes")
+    progress_log("spinning stack down to 0 nodes")
     cfn = boto3.client('cloudformation', region_name=session['region'])
     spindown_parms = [
         { 'ParameterKey': 'ClusterNodeMax', 'ParameterValue': '0' },
@@ -118,7 +162,7 @@ def spindown_to_zero_appnodes():
         spindown_parms.append({ 'ParameterKey': 'SynchronyClusterNodeMax', 'ParameterValue': '0' })
         spindown_parms.append({ 'ParameterKey': 'SynchronyClusterNodeMin', 'ParameterValue': '0' })
 
-    pprint(spindown_parms)
+    progress_log(str(spindown_parms))
 
     update_stack = cfn.update_stack(
         StackName=session['stack'],
@@ -126,12 +170,12 @@ def spindown_to_zero_appnodes():
         UsePreviousTemplate=True,
         Capabilities=[ 'CAPABILITY_IAM' ],
     )
-    pprint(update_stack)
+    progress_log(str(update_stack))
     wait_stackupdate_complete()
     return
 
 def spinup_to_one_appnode():
-    print("spinning stack up to one appnode")
+    progress_log("spinning stack up to one appnode")
     # for connie 1 app node and 1 synchrony
     cfn = boto3.client('cloudformation', region_name=session['region'])
     spinup_parms = [
@@ -146,7 +190,7 @@ def spinup_to_one_appnode():
         spinup_parms.append({ 'ParameterKey': 'SynchronyClusterNodeMax', 'ParameterValue': '1' })
         spinup_parms.append({ 'ParameterKey': 'SynchronyClusterNodeMin', 'ParameterValue': '1' })
 
-    pprint(spinup_parms)
+    progress_log(str(spinup_parms))
 
     update_stack = cfn.update_stack(
         StackName=session['stack'],
@@ -156,38 +200,72 @@ def spinup_to_one_appnode():
     )
     wait_stackupdate_complete()
     validate_service_responding()
-    pprint(update_stack)
+    progress_log(str(update_stack))
+    return
+
+##
+#### Common functions
+##
+def progress_log(log_this):
+    print(str(log_this))
+    session['progress_log'] = "\n".join([str(log_this), str(session['progress_log'])])
     return
 
 def check_stack_state():
-    print(" ==> checking stack state ")
+    progress_log(" ==> checking stack state ")
     cfn = boto3.client('cloudformation', region_name=session['region'])
     stack_state = cfn.describe_stacks(StackName=session['stack'])
     return(stack_state['Stacks'][0]['StackStatus'])
 
 def wait_stackupdate_complete():
-    print("waiting for stack update to complete")
+    progress_log("waiting for stack update to complete")
     stack_state = check_stack_state()
     while stack_state == "UPDATE_IN_PROGRESS":
+        progress_log(str("====> stack_state is: " + stack_state + " waiting .... " + str(datetime.now())))
         time.sleep(30)
         stack_state = check_stack_state()
     return
 
 def check_service_status():
-    print(" ==> checking service status ")
+    progress_log(str(" ==> checking service status at " + session['lburl'] + "/status"))
     service_status = requests.get(session['lburl'] + '/status')
     return(service_status.text)
 
 def validate_service_responding():
-    print("waiting for service to reply RUNNING on /status")
+    progress_log("waiting for service to reply RUNNING on /status")
     service_state = check_service_status()
     while service_state != '{"state":"RUNNING"}' :
+        progress_log(str("====> health check reports: " + service_state + " waiting for RUNNING " + str(datetime.now())))
         time.sleep(60)
-        print("====> health check reports: ",service_state, " waiting for RUNNING ")
-        stack_state = check_service_status()
+        service_state = check_service_status()
     return
 
 def spinup_remaining_nodes():
+    progress_log("spinning up any remaining nodes in stack")
+    # for connie 1 app node and 1 synchrony
+    cfn = boto3.client('cloudformation', region_name=session['region'])
+    spinup_parms = [
+        { 'ParameterKey': 'ClusterNodeMax', 'ParameterValue': session['appnodemax'] },
+        { 'ParameterKey': 'ClusterNodeMin', 'ParameterValue': session['appnodemin'] },
+        { 'ParameterKey': 'CustomDnsName', 'UsePreviousValue': True },
+    ]
+    if 'preupgrade_jira_version' in session:
+        spinup_parms.append({'ParameterKey': 'JiraVersion', 'ParameterValue': session['newversion']})
+    if 'preupgrade_confluence_version' in session:
+        spinup_parms.append({ 'ParameterKey': 'ConfluenceVersion', 'ParameterValue': session['newversion']})
+        spinup_parms.append({ 'ParameterKey': 'SynchronyClusterNodeMax', 'ParameterValue': session['syncnodemax'] })
+        spinup_parms.append({ 'ParameterKey': 'SynchronyClusterNodeMin', 'ParameterValue': session['syncnodemin'] })
+
+    progress_log(str(spinup_parms))
+
+    update_stack = cfn.update_stack(
+        StackName=session['stack'],
+        Parameters=spinup_parms,
+        UsePreviousTemplate=True,
+        Capabilities=[ 'CAPABILITY_IAM' ],
+    )
+    wait_stackupdate_complete()
+    progress_log("stack restored to full node count")
     return
 
 def get_cfn_stacks_for_environment(env):
@@ -201,7 +279,7 @@ def get_cfn_stacks_for_environment(env):
     )
     stack_name_list = []
     for stack in stack_list['StackSummaries']:
-        print(stack['StackName'])
+        progress_log(stack['StackName'])
         stack_name_list.append(stack['StackName'])
     return stack_name_list
 
@@ -304,4 +382,4 @@ def envact(env, action):
 #     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True, debug=True)
