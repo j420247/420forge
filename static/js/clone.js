7200ba4d1b7fed0f061de27e7ea97c6afdf9f66d
Denise Unterwurzacher <dunterwurzacher@atlassian.com>
2018-03-21 13:04:41 -0500

    Moved snapshots into the param list and changed how the list is built. Made snapshots selectable. UI done, next steps are to make the action button actually delete and recreate the stack with the entered parameters.

--- static/js/clone.js
+++ static/js/clone.js
@@ -19,73 +19,8 @@ function selectStack(stackName) {
     var baseUrl = window.location .protocol + "//" + window.location.host;
     var env = 'prod';
 
-    var ebsSnapshotRequest = new XMLHttpRequest();
-    ebsSnapshotRequest.open("GET", baseUrl  + "/getEbsSnapshots/" + stackName, true);
-    ebsSnapshotRequest.setRequestHeader("Content-Type", "text/xml");
-    ebsSnapshotRequest.onreadystatechange = function () {
-        if (ebsSnapshotRequest.readyState === XMLHttpRequest.DONE && ebsSnapshotRequest.status === 200) {
-            var ebsSnapDropdown = document.getElementById("ebsSnapshots");
-            while (ebsSnapDropdown.firstChild) {
-                ebsSnapDropdown.removeChild(ebsSnapDropdown.firstChild);
-            }
-
-            var ebsSnaps = JSON.parse(ebsSnapshotRequest.responseText);
-            ebsSnaps.sort(function(a, b) {
-                return - a.localeCompare(b)});
-
-            for (var snap in ebsSnaps) {
-                console.log(ebsSnaps[snap]);
-                var li = document.createElement("LI");
-                var text = document.createTextNode(ebsSnaps[snap]);
-                text.className = "selectEbsSnapshotOption";
-                li.appendChild(text);
-                ebsSnapDropdown.appendChild(li);
-            }
-
-            var ebsSnaps = document.getElementsByClassName("selectEbsSnapshotOption");
-            for (var i = 0; i < ebsSnaps.length; i++) {
-                ebsSnaps[i].addEventListener("click", function (data) {
-                    var ebsSnap = data.target.text;
-                    selectEbsSnap(ebsSnap);
-                }, false);
-            }
-        }
-    };
-    ebsSnapshotRequest.send();
-
-    var rdsSnapshotRequest = new XMLHttpRequest();
-    rdsSnapshotRequest.open("GET", baseUrl  + "/getRdsSnapshots/" + stackName, true);
-    rdsSnapshotRequest.setRequestHeader("Content-Type", "text/xml");
-    rdsSnapshotRequest.onreadystatechange = function () {
-        if (rdsSnapshotRequest.readyState === XMLHttpRequest.DONE && rdsSnapshotRequest.status === 200) {
-            var rdsSnapDropdown = document.getElementById("rdsSnapshots");
-            while (rdsSnapDropdown.firstChild) {
-                rdsSnapDropdown.removeChild(rdsSnapDropdown.firstChild);
-            }
-
-            var rdsSnaps = JSON.parse(rdsSnapshotRequest.responseText);
-            rdsSnaps.sort(function(a, b) {
-                return - a.localeCompare(b)});
-
-            for (var snap in rdsSnaps) {
-                console.log(rdsSnaps[snap]);
-                var li = document.createElement("LI");
-                var text = document.createTextNode(rdsSnaps[snap]);
-                text.className = "selectRdsSnapshotOption";
-                li.appendChild(text);
-                rdsSnapDropdown.appendChild(li);
-            }
-
-            var rdsSnaps = document.getElementsByClassName("selectRdsSnapshotOption");
-            for (var i = 0; i < rdsSnaps.length; i++) {
-                rdsSnaps[i].addEventListener("click", function (data) {
-                    var rdsSnaps = data.target.text;
-                    selectRdsSnap(rdsSnaps);
-                }, false);
-            }
-        }
-    };
-    rdsSnapshotRequest.send();
+    getEbsSnapshots(baseUrl, stackName);
+    getRdsSnapshots(baseUrl, stackName);
 
     var stackParamsRequest = new XMLHttpRequest();
     stackParamsRequest.open("GET", baseUrl  + "/stackParams/" + env + "/" + stackName, true);
@@ -97,17 +32,9 @@ function selectStack(stackName) {
             params.sort(function(a, b) {
                 return a.ParameterKey.localeCompare(b.ParameterKey)});
 
-            $("#paramsForm").html("");
-            $("#paramsForm").className = "aui long-label";
-
+            $("#paramsList").html("");
             var fieldset = document.createElement("FIELDSET");
             fieldset.id = "fieldSet";
-
-            var stackNameParam = {
-                "ParameterKey": "StackName",
-                "ParameterValue": ""
-            };
-            createInputParameter(stackNameParam, fieldset);
 
             for (var param in params) {
                 createInputParameter(params[param], fieldset);
@@ -116,8 +43,8 @@ function selectStack(stackName) {
                 }
             }
 
-            var paramsForm = document.getElementById("paramsForm");
-            paramsForm.appendChild(fieldset);
+            var paramsList = document.getElementById("paramsList");
+            paramsList.appendChild(fieldset);
 
             document.getElementById("CatalinaOptsVal").value += "-Datlassian.mail.senddisabled=true " +
                 "-Datlassian.mail.fetchdisabled=true " +
@@ -128,31 +55,97 @@ function selectStack(stackName) {
         }
     };
     stackParamsRequest.send();
-
-    function createInputParameter(param, fieldset) {
-        var div = document.createElement("DIV");
-        div.className = "field-group";
-        div.id = "fieldGroup";
-
-        var label = document.createElement("LABEL");
-        label.htmlFor = param.ParameterKey + "Val";
-        label.innerHTML = param.ParameterKey;
-        div.appendChild(label);
-
-        var input = document.createElement("INPUT");
-        input.className = "text";
-        input.id = param.ParameterKey + "Val";
-        input.value = param.ParameterValue;
-        div.appendChild(input);
-
-        fieldset.appendChild(div);
-    }
 }
 
-function selectEbsSnap(ebsSnap) {
-    $("#ebsSnapshotSelector").text(ebsSnap);
+function createInputParameter(param, fieldset) {
+    var div = document.createElement("DIV");
+    div.className = "field-group";
+    div.id = "fieldGroup";
+
+    var label = document.createElement("LABEL");
+    label.htmlFor = param.ParameterKey + "Val";
+    label.innerHTML = param.ParameterKey;
+    div.appendChild(label);
+
+    var input = document.createElement("INPUT");
+    input.className = "text";
+    input.id = param.ParameterKey + "Val";
+    input.value = param.ParameterValue;
+    div.appendChild(input);
+
+    fieldset.appendChild(div);
 }
 
-function selectRdsSnap(rdsSnap) {
-    $("#rdsSnapshotSelector").text(rdsSnap);
+function getEbsSnapshots(baseUrl, stackName) {
+    var ebsSnapshotRequest = new XMLHttpRequest();
+    ebsSnapshotRequest.open("GET", baseUrl + "/getEbsSnapshots/" + stackName, true);
+    ebsSnapshotRequest.setRequestHeader("Content-Type", "text/xml");
+    ebsSnapshotRequest.onreadystatechange = function () {
+        if (ebsSnapshotRequest.readyState === XMLHttpRequest.DONE && ebsSnapshotRequest.status === 200) {
+            var ebsSnapDropdown = document.getElementById("ebsSnapshots");
+            while (ebsSnapDropdown.firstChild) {
+                ebsSnapDropdown.removeChild(ebsSnapDropdown.firstChild);
+            }
+
+            var ebsSnaps = JSON.parse(ebsSnapshotRequest.responseText);
+            ebsSnaps.sort(function (a, b) {
+                return -a.localeCompare(b)
+            });
+
+            for (var snap in ebsSnaps) {
+                var li = document.createElement("LI");
+                var anchor = document.createElement("A");
+                anchor.className = "selectEbsSnapshotOption";
+                var text = document.createTextNode(ebsSnaps[snap]);
+                anchor.appendChild(text)
+                li.appendChild(anchor);
+                ebsSnapDropdown.appendChild(li);
+            }
+
+            var ebsSnaps = document.getElementsByClassName("selectEbsSnapshotOption");
+            for (var i = 0; i < ebsSnaps.length; i++) {
+                ebsSnaps[i].addEventListener("click", function (data) {
+                    $("#ebsSnapshotSelector").text(data.target.text);
+                }, false);
+            }
+        }
+    };
+    ebsSnapshotRequest.send();
+}
+
+function getRdsSnapshots(baseUrl, stackName) {
+    var rdsSnapshotRequest = new XMLHttpRequest();
+    rdsSnapshotRequest.open("GET", baseUrl + "/getRdsSnapshots/" + stackName, true);
+    rdsSnapshotRequest.setRequestHeader("Content-Type", "text/xml");
+    rdsSnapshotRequest.onreadystatechange = function () {
+        if (rdsSnapshotRequest.readyState === XMLHttpRequest.DONE && rdsSnapshotRequest.status === 200) {
+            var rdsSnapDropdown = document.getElementById("rdsSnapshots");
+            while (rdsSnapDropdown.firstChild) {
+                rdsSnapDropdown.removeChild(rdsSnapDropdown.firstChild);
+            }
+
+            var rdsSnaps = JSON.parse(rdsSnapshotRequest.responseText);
+            rdsSnaps.sort(function (a, b) {
+                return -a.localeCompare(b)
+            });
+
+            for (var snap in rdsSnaps) {
+                var li = document.createElement("LI");
+                var anchor = document.createElement("A");
+                anchor.className = "selectRdsSnapshotOption";
+                var text = document.createTextNode(rdsSnaps[snap]);
+                anchor.appendChild(text);
+                li.appendChild(anchor);
+                rdsSnapDropdown.appendChild(li);
+            }
+
+            var rdsSnaps = document.getElementsByClassName("selectRdsSnapshotOption");
+            for (var i = 0; i < rdsSnaps.length; i++) {
+                rdsSnaps[i].addEventListener("click", function (data) {
+                    $("#rdsSnapshotSelector").text(data.target.text);
+                }, false);
+            }
+        }
+    };
+    rdsSnapshotRequest.send();
 }
