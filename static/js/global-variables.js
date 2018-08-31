32d96b38005a7cf16fa19003c442b62b3144a469
Denise Unterwurzacher <dunterwurzacher@atlassian.com>
2018-08-31 14:18:23 +1000

    ITOPS-669 .gitignore will not ignore changes to this file - re-committing example version

--- static/js/global-variables.js
+++ static/js/global-variables.js
@@ -1,22 +1,16 @@
-// These values can be used to automatically populate template parameters
-// This means you don't have to always copy/paste VPCs, subnets, hostedzones or ssh keys into the fields
-
 // VPCs
-// format vpc-56abc789
-const us_east_1_default_vpc = "vpc-320c1355";
-const us_west_2_default_vpc = "vpc-dd8dc7ba";
-const lab_default_vpc = "vpc-ff1b9284";
+// format vpc-abc56789
+const us_east_1_default_vpc = "";
+const us_west_2_default_vpc = "";
 
 // Subnets
-// format 'subnet-12abc345,subnet-12abc346'
-const us_east_1_default_subnets = "subnet-df0c3597,subnet-f1fb87ab";
-const us_west_2_default_subnets = "subnet-eb952fa2,subnet-f2bddd95";
-const lab_dmz_default_subnets = "subnet-a2b3a3c6,subnet-a9b08f86";
-const lab_private_default_subnets = "subnet-d9162484,subnet-158d4b5f";
+// format 'subnet-abc12345,subnet-abc12346'
+const us_east_1_default_subnets = "";
+const us_west_2_default_subnets = "";
 
 // Hosted Zone
-// format 'myteam.example.com.'
-const hosted_zone = "wpt.atlassian.com.";
+// format 'myteam.exampe.com.'
+const hosted_zone = "";
 
 // SSH Key
-const ssh_key_name = "WPE-GenericKeyPair-20161102";
+const ssh_key_name = "";
