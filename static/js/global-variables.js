a446e06d89fc3d1e4136d2c6a663c6b1138ff4b8
Denise Unterwurzacher <dunterwurzacher@atlassian.com>
2018-08-31 14:19:56 +1000

    ITOPS-669 .gitignore will not ignore changes to this file - re-committing example version

--- static/js/global-variables.js
+++ static/js/global-variables.js
@@ -1,15 +1,18 @@
+// These values can be used to automatically populate template parameters
+// This means you don't have to always copy/paste VPCs, subnets, hostedzones or ssh keys into the fields
+
 // VPCs
-// format vpc-abc56789
+// format vpc-56abc789
 const us_east_1_default_vpc = "";
 const us_west_2_default_vpc = "";
 
 // Subnets
-// format 'subnet-abc12345,subnet-abc12346'
+// format 'subnet-12abc345,subnet-12abc346'
 const us_east_1_default_subnets = "";
 const us_west_2_default_subnets = "";
 
 // Hosted Zone
-// format 'myteam.exampe.com.'
+// format 'myteam.example.com.'
 const hosted_zone = "";
 
 // SSH Key
