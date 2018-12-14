ca2448cc797392b4629708b964421a8700074640
Denise Unterwurzacher <dunterwurzacher@atlassian.com>
2018-12-14 17:01:17 -0600

    ITOPS-1077 Get all RDS snapshots if there are more than 100

--- acforge.py
+++ acforge.py
@@ -656,14 +656,20 @@ class getEbsSnapshots(Resource):
 class getRdsSnapshots(Resource):
     def get(self, region, stack_name):
         rds = boto3.client('rds', region_name=region)
+        snapshotIds = []
         try:
-            snapshots = rds.describe_db_snapshots(DBInstanceIdentifier=stack_name)
+            # get snapshots and append ids to list
+            snapshots_response = rds.describe_db_snapshots(DBInstanceIdentifier=stack_name)
+            for snap in snapshots_response['DBSnapshots']:
+                snapshotIds.append(str(snap['SnapshotCreateTime']).split(' ').__getitem__(0) + ": " + snap['DBSnapshotIdentifier'])
+            # if there are more than 100 snapshots the response will contain a marker, get the next lot of snapshots and add them to the list
+            while 'Marker' in snapshots_response:
+                snapshots_response = rds.describe_db_snapshots(DBInstanceIdentifier=stack_name, Marker=snapshots_response['Marker'])
+                for snap in snapshots_response['DBSnapshots']:
+                    snapshotIds.append(str(snap['SnapshotCreateTime']).split(' ').__getitem__(0) + ": " + snap['DBSnapshotIdentifier'])
         except botocore.exceptions.ClientError as e:
             print(e.args[0])
             return
-        snapshotIds = []
-        for snap in snapshots['DBSnapshots']:
-            snapshotIds.append(str(snap['SnapshotCreateTime']).split(' ').__getitem__(0) + ": " + snap['DBSnapshotIdentifier'])
         snapshotIds.sort(reverse=True)
         return snapshotIds
 
