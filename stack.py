bd476ac6a085246679b07e88dbebc845ebb6a4c0
Denise Unterwurzacher <dunterwurzacher@atlassian.com>
2019-01-04 17:54:50 -0600

    ITOPS-1223 Fix for upgrading non-confluence stacks

--- stack.py
+++ stack.py
@@ -273,13 +273,13 @@ class Stack:
             print(e.args[0])
         return "Timed Out"
 
-    def spinup_remaining_nodes(self, app_type, app_node_count, synchrony_node_count):
+    def spinup_remaining_nodes(self, app_type, app_node_count, synchrony_node_count=False):
         self.log_msg(log.INFO, 'Spinning up any remaining nodes in stack')
         cfn = boto3.client('cloudformation', region_name=self.region)
         spinup_parms = self.getparms()
         spinup_parms = self.update_parmlist(spinup_parms, 'ClusterNodeMax', app_node_count)
         spinup_parms = self.update_parmlist(spinup_parms, 'ClusterNodeMin', app_node_count)
-        if app_type == 'confluence':
+        if synchrony_node_count:
             spinup_parms = self.update_parmlist(spinup_parms, 'SynchronyClusterNodeMax', synchrony_node_count)
             spinup_parms = self.update_parmlist(spinup_parms, 'SynchronyClusterNodeMin', synchrony_node_count)
         try:
@@ -510,9 +510,10 @@ class Stack:
             return False
         # spinup remaining nodes in stack if needed
         if preupgrade_app_node_count > "1":
-            self.spinup_remaining_nodes(app_type, preupgrade_app_node_count, preupgrade_synchrony_node_count)
-        elif preupgrade_synchrony_node_count > "1":
-            self.spinup_remaining_nodes(app_type, preupgrade_app_node_count, preupgrade_synchrony_node_count)
+            if app_type.lower() == 'confluence':
+                self.spinup_remaining_nodes(app_type, preupgrade_app_node_count, preupgrade_synchrony_node_count)
+            else:
+                self.spinup_remaining_nodes(app_type, preupgrade_app_node_count)
         # TODO wait for remaining nodes to respond ??? ## maybe a LB check for active node count
         # TODO enable traffic at VTM
         self.log_msg(log.INFO, f'Upgrade successful for {self.stack_name} at {self.region} to version {new_version}')
