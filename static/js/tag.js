edd5fbd895a07107bcce8dceb00b76565946b523
Denise Unterwurzacher <dunterwurzacher@atlassian.com>
2019-01-29 16:13:17 -0800

    ITOPS-900 Refactored tag

--- static/js/tag.js
+++ static/js/tag.js
@@ -1,5 +1,3 @@
-var stack_name;
-
 function onReady() {
     $("#stackInformation").hide();
     $("#tags").hide();
@@ -9,93 +7,88 @@ function onReady() {
         stacks[i].addEventListener("click", function (data) {
             $("#stackInformation").show();
             $("#tags").show();
-            stack_name = data.target.text;
+            var stack_name = data.target.text;
             selectStack(stack_name);
             getTags(stack_name);
         }, false);
     }
 
     var actionButton = document.getElementById("action-button");
-    actionButton.removeEventListener("click", defaultActionBtnEvent);
     actionButton.addEventListener("click", function (data) {
         sendTagsAsJson();
     });
 }
 
-function getTags() {
+function getTags(stack_name) {
+    send_http_get_request(baseUrl + "/getTags/" + region + "/" + stack_name, displayTags);
+}
+
+function displayTags(responseText) {
+    var tags = JSON.parse(responseText);
     var tagsList = document.getElementById("existing-tags");
     tagsList.innerHTML = "<h3>Existing tags</h3>";
 
-    var getTagsRequest = new XMLHttpRequest();
-    getTagsRequest.open("GET", baseUrl + "/getTags/" + region + "/" + stack_name, true);
-    getTagsRequest.setRequestHeader("Content-Type", "text/xml");
-    getTagsRequest.onreadystatechange = function () {
-        if (getTagsRequest.readyState === XMLHttpRequest.DONE && getTagsRequest.status === 200) {
-            tags = JSON.parse(getTagsRequest.responseText);
+    // check for errors or no tags
+    if (tags.length === 0) {
+        var label = document.createElement("LABEL");
+        label.innerText = "None";
+        tagsList.appendChild(label);
+        return;
+    }
+    if (tags[0].error) {
+        var label = document.createElement("LABEL");
+        label.innerText = tag[0].error;
+        tagsList.appendChild(label);
+        return;
+    }
 
-            // check for errors or no tags
-            if (tags.length === 0) {
-                var label = document.createElement("LABEL");
-                label.innerText = "None";
-                tagsList.appendChild(label);
-                return;
+    // display each tag
+    for (var tag in tags) {
+        var div = document.createElement("DIV");
+        div.className = "field-group tag-field-group";
+
+        var keyInput = document.createElement("INPUT");
+        keyInput.className = "text";
+        keyInput.type = "text";
+        keyInput.name = "key";
+        keyInput.value = tags[tag].Key;
+
+        var valInput = document.createElement("INPUT");
+        valInput.className = "text";
+        valInput.type = "text";
+        valInput.name = "value";
+        valInput.value = tags[tag].Value;
+
+        var delBtn = document.createElement("SPAN");
+        delBtn.innerHTML = "<span class=\"aui-icon aui-icon-small aui-iconfont-delete\">Delete</span>";
+        delBtn.addEventListener("click", function (data) {
+            var val = this.previousSibling;
+            var key = val.previousSibling;
+            if (key.style.getPropertyValue("text-decoration") !== "line-through") {
+                key.style.setProperty("text-decoration", "line-through");
+                key.disabled = true;
+                val.style.setProperty("text-decoration", "line-through");
+                val.disabled = true;
+                this.innerHTML = "<span class=\"aui-icon aui-icon-small aui-iconfont-undo\">Delete</span>";
+            } else {
+                key.style.removeProperty("text-decoration");
+                key.disabled = false;
+                val.style.removeProperty("text-decoration");
+                val.disabled = false;
+                this.innerHTML = "<span class=\"aui-icon aui-icon-small aui-iconfont-delete\">Delete</span>";
             }
-            if (tags[0].error) {
-                var label = document.createElement("LABEL");
-                label.innerText = tag[0].error;
-                tagsList.appendChild(label);
-                return;
-            }
+        });
 
-            // display each tag
-            for (var tag in tags) {
-                var div = document.createElement("DIV");
-                div.className = "field-group tag-field-group";
+        div.appendChild(keyInput);
+        div.appendChild(valInput);
+        div.appendChild(delBtn);
 
-                var keyInput = document.createElement("INPUT");
-                keyInput.className = "text";
-                keyInput.type = "text";
-                keyInput.name = "key";
-                keyInput.value = tags[tag].Key;
-
-                var valInput = document.createElement("INPUT");
-                valInput.className = "text";
-                valInput.type = "text";
-                valInput.name = "value";
-                valInput.value = tags[tag].Value;
-
-                var delBtn = document.createElement("SPAN");
-                delBtn.innerHTML = "<span class=\"aui-icon aui-icon-small aui-iconfont-delete\">Delete</span>";
-                delBtn.addEventListener("click", function (data) {
-                    var val = this.previousSibling;
-                    var key = val.previousSibling;
-                    if (key.style.getPropertyValue("text-decoration") !== "line-through") {
-                        key.style.setProperty("text-decoration", "line-through");
-                        key.disabled = true;
-                        val.style.setProperty("text-decoration", "line-through");
-                        val.disabled = true;
-                        this.innerHTML = "<span class=\"aui-icon aui-icon-small aui-iconfont-undo\">Delete</span>";
-                    } else {
-                        key.style.removeProperty("text-decoration");
-                        key.disabled = false;
-                        val.style.removeProperty("text-decoration");
-                        val.disabled = false;
-                        this.innerHTML = "<span class=\"aui-icon aui-icon-small aui-iconfont-delete\">Delete</span>";
-                    }
-                });
-
-                div.appendChild(keyInput);
-                div.appendChild(valInput);
-                div.appendChild(delBtn);
-
-                tagsList.appendChild(div);
-            }
-        }
-    };
-    getTagsRequest.send();
+        tagsList.appendChild(div);
+    }
 }
 
 function sendTagsAsJson() {
+    var stack_name = scrapePageForStackName();
     var tagsArray = [];
     var tags = document.getElementsByClassName("tag-field-group");
 
@@ -114,11 +107,6 @@ function sendTagsAsJson() {
             tagsArray.push(jsonParam);
         }
     }
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", baseUrl + "/do" + action + "/" + region + "/" + stack_name, true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.send(JSON.stringify(tagsArray));
-
+    send_http_post_request(baseUrl + "/dotag/" + region + "/" + stack_name, JSON.stringify(tagsArray));
     redirectToLog(stack_name);
 }
