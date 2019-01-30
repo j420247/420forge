6a4767a88808f7d8c26800db93fa24c030690368
Denise Unterwurzacher <dunterwurzacher@atlassian.com>
2019-01-30 13:30:12 -0800

    ITOPS-900 Refactor upgrade action

--- static/js/upgrade.js
+++ static/js/upgrade.js
@@ -40,24 +40,18 @@ function checkZDUCompatibility(stack_name) {
     $("#zduCheckLbl").text("Checking Zero Downtime Upgrade compatibility ...");
     $("#performZDUDiv").attr("style", "display:none");
 
-    var zduCompatibilityRequest = new XMLHttpRequest();
-    zduCompatibilityRequest.open("GET", baseUrl + "/getZDUCompatibility/" + region + "/" + stack_name, true);
-    zduCompatibilityRequest.setRequestHeader("Content-Type", "text/xml");
-    zduCompatibilityRequest.addEventListener("load", processResponse);
-    zduCompatibilityRequest.onreadystatechange = function () {
-        if (zduCompatibilityRequest.readyState === XMLHttpRequest.DONE && zduCompatibilityRequest.status === 200) {
-            var compatibility = JSON.parse(zduCompatibilityRequest.responseText);
-            if (compatibility === true) {
-                $("#zduCheckDiv").attr("style", "display:none");
-                $("#performZDUDiv").removeAttr("style");
-            } else {
-                $("#zduCheckLbl").text("Stack is not ZDU compatible: " + compatibility);
-            }
-            $("#action-button").removeAttr("aria-disabled");
+    send_http_get_request(baseUrl + "/getZDUCompatibility/" + region + "/" + stack_name, displayZDUCompatibility);
+}
 
-        }
-    };
-    zduCompatibilityRequest.send();
+function displayZDUCompatibility(responseText) {
+    var compatibility = JSON.parse(responseText);
+    if (compatibility === true) {
+        $("#zduCheckDiv").attr("style", "display:none");
+        $("#performZDUDiv").removeAttr("style");
+    } else {
+        $("#zduCheckLbl").text("Stack is not ZDU compatible: " + compatibility);
+    }
+    $("#action-button").removeAttr("aria-disabled");
 }
 
 function beginUpgrade() {
@@ -74,15 +68,7 @@ function performUpgrade() {
     var zdu = document.getElementById("zduCheckBox").checked;
     var version = $("#upgradeVersionSelector").val();
     var stack_name = scrapePageForStackName();
-    var url = baseUrl + "/do" + action + "/" + region + "/" + stack_name;
-
-    var upgradeRequest = new XMLHttpRequest();
-    url += "/" + version + "/" + zdu;
-
-    upgradeRequest.open("POST", url, true);
-    upgradeRequest.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    upgradeRequest.addEventListener("load", processResponse);
-    upgradeRequest.send(JSON.stringify(zdu ? getAuthDetailsAsJSON() : [{}]));
-
+    var url = baseUrl + "/doupgrade/" + region + "/" + stack_name + "/" + version + "/" + zdu;
+    send_http_post_request(url, JSON.stringify(zdu ? getAuthDetailsAsJSON() : [{}]));
     redirectToLog(stack_name);
 }
