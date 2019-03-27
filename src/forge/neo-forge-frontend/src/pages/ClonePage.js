e22c319a01cf1d7f15a7da2a2da8f0954522655c
Ben Partridge <bpartridge@atlassian.com>
2019-03-27 14:28:25 +1100

    Implement form loading and rendering

--- src/forge/neo-forge-frontend/src/pages/ClonePage.js
+++ src/forge/neo-forge-frontend/src/pages/ClonePage.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Page, { Grid, GridColoumn } from '@atlaskit/page';
 import PageHeader from '@atlaskit/page-header';
-import Form, { Field } from '@atlaskit/form';
+import Form, { HelperMessage, Field } from '@atlaskit/form';
 import Spinner from '@atlaskit/spinner';
+import TextField from '@atlaskit/textfield';
+import Select from '@atlaskit/select';
 import PropTypes from 'prop-types';
 
 import { getTemplateParamsForTemplate } from '../modules/api';
@@ -15,7 +17,7 @@ export default class ClonePage extends Component {
 
   state = {
     templateLoading: false,
-    templateParameters: {},
+    templateParameters: [],
   }
 
   componentDidMount() {
@@ -28,23 +30,71 @@ export default class ClonePage extends Component {
     .then(resJson => this.setState({ templateParameters: resJson, templateLoading: false }));
   }
 
+  makeCfnParamater(cfnParameter) {
+    return cfnParameter.AllowedValues ? this.makeCfnParameterSelectInput(cfnParameter) : this.makeCfnParameterTextInput(cfnParameter);
+  }
+
+  makeCfnParameterSelectInput(cfnParameter) {
+    const selectOptions = cfnParameter.AllowedValues.map(value => ({ label: `${value}`, value: `${value}`}));
+    return (<Field
+      name={cfnParameter.ParameterKey}
+      label={cfnParameter.ParameterKey}
+      key={cfnParameter.ParameterKey}
+      defaultValue={selectOptions.find(option => option.value === `${cfnParameter.ParameterValue}`)}
+    >
+      {({ fieldProps, error, meta }) => (
+          <Fragment>
+          <Select {...fieldProps}
+            options={selectOptions}
+          />
+        </Fragment>
+      )}
+    </Field>
+    );
+  }
+
+  makeCfnParameterTextInput(cfnParameter) {
+    return (<Field
+      name={cfnParameter.ParameterKey}
+      label={cfnParameter.ParameterKey}
+      key={cfnParameter.ParameterKey}
+      defaultValue={cfnParameter.ParameterValue}
+    >
+      {({ fieldProps, error, meta }) => (
+          <Fragment>
+          <TextField {...fieldProps}/>
+          {!error && (
+            <HelperMessage>
+              {cfnParameter.ParameterDescription}
+            </HelperMessage>
+          )}
+        </Fragment>
+      )}
+    </Field>
+    );
+  }
+
   render() {
     const { currentStack } = this.props;
     const { templateLoading, templateParameters } = this.state;
     return (
       <Page>
-        <PageHeader>Clone an Atlassian Product</PageHeader>
-        { templateLoading ?
-          <Spinner size="large" />
-          :
-          <Form onSubmit={data => console.log('form data', data)}>
-            {({ formProps }) => (
-              <form {...formProps}>
-
-              </form>
-            )}
-          </Form>
-        }
+        <Grid>
+          <PageHeader>Clone an Atlassian Product</PageHeader>
+          { templateLoading ?
+            <Spinner size="large" />
+            :
+            <Form onSubmit={data => console.log('form data', data)}>
+              {({ formProps }) => (
+                <form {...formProps}>
+                {
+                  templateParameters.map(parameter => this.makeCfnParamater(parameter))
+                }
+                </form>
+              )}
+            </Form>
+          }
+        </Grid>
       </Page>
     )
   }
