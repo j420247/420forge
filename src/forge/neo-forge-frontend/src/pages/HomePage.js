345eb38b0ab56a1cbcf85c23e81beb4ccb0924bc
michaelkemp <mkemp@atlassian.com>
2019-03-28 13:07:20 +1100

    Add homepage to neo-forge-frontend

--- src/forge/neo-forge-frontend/src/pages/HomePage.js
+++ src/forge/neo-forge-frontend/src/pages/HomePage.js
@@ -1,33 +1,18 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import Button, { ButtonGroup } from '@atlaskit/button';
-import MainSection from '../components/MainSection';
-import ContentWrapper from '../components/ContentWrapper';
-import PageTitle from '../components/PageTitle';
+import React from "react";
+import ContentWrapper from "../components/ContentWrapper";
+import PageTitle from "../components/PageTitle";
 
-export default class HomePage extends Component {
-  static contextTypes = {
-    showModal: PropTypes.func,
-    addFlag: PropTypes.func,
-    onConfirm: PropTypes.func,
-    onCancel: PropTypes.func,
-    onClose: PropTypes.func,
-  };
+const HomePage = () => (
+  <ContentWrapper>
+    <PageTitle>Forge</PageTitle>
+    <section style={{ marginBottom: "10px" }}>
+      <p>
+        This is a tool to help you manage your Atlassian (server or data centre)
+        application instances.
+      </p>
+      <p>To get started select a stack to manage on the left.</p>
+    </section>
+  </ContentWrapper>
+);
 
-  render() {
-    return (
-      <ContentWrapper>
-        <PageTitle>Home</PageTitle>
-        <MainSection />
-        <ButtonGroup>
-          <Button
-            appearance="primary"
-            onClick={this.context.showModal}
-            onClose={() => { }}
-          >Click to view Atlaskit modal</Button>
-          <Button onClick={this.context.addFlag}>click to view Atlaskit flag</Button>
-        </ButtonGroup>
-      </ContentWrapper>
-    );
-  }
-}
+export default HomePage;
