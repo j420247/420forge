00dfa066a6aadae2459bb7bb6dbdf58ea0a2cf9c
Geoff Jacobs <gjacobs@atlassian.com>
2019-04-11 08:02:27 +1000

    ITOPS-1548 reverting __version__ changes

--- src/forge/version.py
+++ src/forge/version.py
@@ -1,8 +1,1 @@
-import git
-from pathlib import Path
-from os.path import dirname
-from flask import current_app
-
-repo = git.Repo(Path(dirname(dirname(current_app.root_path))))
-
-__version__ = f'{repo.active_branch.name}'
+__version__ = '2.0.0'
