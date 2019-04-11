40dd492fbdb89fb681efaab346a83eca50b3dc4e
Brett Meehan <bmeehan@atlassian.com>
2019-04-12 08:02:03 +1000

    ITOPS-1560 add pytest to requirements and more details in run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash 
 
-python -m pytest tests/unit
+# To run tests from your shell first `pipenv shell`
+# note you will need `cloudtoken -d` running for the testing to work locally
+
+# to get less warningy output do `python -m pytest -v tests/unit --disable-warnings`
+
+python -m pytest -v tests/unit
