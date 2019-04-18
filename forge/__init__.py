a36f84d3c2ce46b34bb0646a99f41f2f8f7e5738
Geoff Jacobs <gjacobs@atlassian.com>
2019-04-19 08:19:16 +1000

    ITOPS-1548 fixing merge conflicts

--- forge/__init__.py
+++ forge/__init__.py
@@ -3,22 +3,44 @@ import boto3
 import botocore
 from forge.version import __version__
 from forge.config import config
+from logging.handlers import RotatingFileHandler
+import logging
+from dotenv import load_dotenv
+import os
+from os import getenv
 
 # Import Blueprints
 from forge.api import api_blueprint
 from forge.aws_cfn_stack import aws_cfn_stack_blueprint
 from forge.main import main as main_blueprint
 from forge.saml_auth import saml_blueprint, saml_auth
-from dotenv import load_dotenv
-from os import getenv
+
+log = logging.getLogger('app_log')
 
 
 def create_app(config_class):
+    # create log directory
+    log_dir = 'logs'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # init loggers
+    access_log_handler = RotatingFileHandler(f'{log_dir}/forge_access.log', maxBytes=10000000, backupCount=5)
+    access_log_handler.setLevel(logging.INFO)
+    access_log = logging.getLogger('werkzeug')
+    access_log.addHandler(access_log_handler)
+
+    app_log_handler = RotatingFileHandler(f'{log_dir}/forge.log', maxBytes=10000000, backupCount=5)
+    app_log_handler.setLevel(logging.INFO)
+    log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', '%Y-%m-%d %H:%M:%S')
+    app_log_handler.setFormatter(log_formatter)
+    app_log = logging.getLogger('app_log')
+    app_log.addHandler(app_log_handler)
 
     # load .env file
     load_dotenv()
     # create and initialize app
-    print(f'Starting Atlassian CloudFormation Forge v{__version__}')
+    log.info(f'Starting Atlassian CloudFormation Forge v{__version__}')
     app = Flask(__name__)
     app.config.from_object(config_class)
 
@@ -29,15 +51,15 @@ def create_app(config_class):
         key = ssm_client.get_parameter(Name='atl_forge_secret_key', WithDecryption=True)
         app.config['SECRET_KEY'] = key['Parameter']['Value']
     except botocore.exceptions.NoCredentialsError as e:
-        print('No credentials - please authenticate with Cloudtoken')
+        log.error('No credentials - please authenticate with Cloudtoken')
     except Exception:
-        print('No secret key in parameter store')
+        log.error('No secret key in parameter store')
 
     # create SAML URL if saml enabled
     if not getenv('NO_SAML'):
         saml_auth.configure_saml(ssm_client, app)
     else:
-        print('SAML auth is not configured')
+        log.info('SAML auth is not configured')
 
     # Register Blueprints
     app.register_blueprint(api_blueprint)
